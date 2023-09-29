6f8cbb4941ae2fb9c9e614f88d2f92c61a5f8bee
Pablo Ortiz <paortiz@elpoli.edu.co>
2023-09-29 07:41:22 -0500

    Pretty print of polynomial

--- poly_operations.py
+++ poly_operations.py
@@ -1,6 +1,25 @@
 # importing package
 import numpy
 
+def print_polynomial(list_coeff):
+    """
+    This function prints the polynomial in a readable format.
+    """
+
+    i = len(list_coeff)-1
+    pol_str =""
+    while i >=0:
+        if list_coeff[i] >0:
+            if len(list_coeff) - i != 1:
+                pol_str += " + "
+        elif list_coeff[i] <0:
+            pol_str += " - "
+        if list_coeff[i] !=0:
+            pol_str += str(abs(list_coeff[i])) + "X**" + str(i)
+        i = i-1
+    return pol_str
+
+
 # define the polynomials
 # p(x) = 5(x**2) + (-2)x +5
 
@@ -30,10 +49,28 @@ print(poly_subtraction)
 print()
 
 # divide the polynomials
-poly_division = numpy.polynomial.polynomial.polydiv(px, qx)
+quotient, remainder = numpy.polynomial.polynomial.polydiv(px, qx)
 print("The result of the polynomial division is... ")
-print(poly_division)
-print()
+print(f"Quotient = {quotient}")
+print(f"Remainder = {remainder}")
+
+# p(x) = 4(x**3) + 0x**2 + 3x - 8
+px = (-8, 3, 0, 4)
+
+# q(x) = x + 2
+qx = (2, 1, 0)
+
+print("p(x) = ", print_polynomial(px))
+print("q(x) = ", print_polynomial(qx))
+
+
+
+# divide the polynomials
+quotient, remainder = numpy.polynomial.polynomial.polydiv(px, qx)
+print("The result of the polynomial division is... ")
+print(f"Quotient = {print_polynomial(quotient)}")
+print("Remainder = ",print_polynomial(remainder) + " / " + print_polynomial(qx) )
+
 
 
 
