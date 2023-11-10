89230fecac0b315464283f1c6e2a8f38f9b409d7
Pablo Ortiz <paortiz@elpoli.edu.co>
2023-11-10 16:08:16 -0500

    Deletion node from BST

--- bst.py
+++ bst.py
@@ -126,6 +126,64 @@ def print_non_leaf_node(root):
     print_non_leaf_node(root.left)
     print_non_leaf_node(root.right)
 
+# Function that returns the node with minimum
+# key value found in that tree
+def min_value_node(node):
+    current = node
+ 
+    # Loop down to find the leftmost leaf
+    while current and current.left is not None:
+        current = current.left
+ 
+    return current
+
+# Function that deletes the key and
+# returns the new root
+def delete_node(root, key):
+    # base Case
+    if root is None:
+        return root
+ 
+    # If the key to be deleted is
+    # smaller than the root's key,
+    # then it lies in left subtree
+    if key < root.key:
+        root.left = delete_node(root.left, key)
+ 
+    # If the key to be deleted is
+    # greater than the root's key,
+    # then it lies in right subtree
+    elif key > root.key:
+        root.right = delete_node(root.right, key)
+ 
+    # If key is same as root's key,
+    # then this is the node
+    # to be deleted
+    else:
+        # Node with only one child
+        # or no child
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+ 
+        # Node with two children:
+        # Get the inorder successor(smallest
+        # in the right subtree)
+        temp = min_value_node(root.right)
+ 
+        # Copy the inorder successor's
+        # content to this node
+        root.key = temp.key
+ 
+        # Delete the inorder successor
+        root.right = delete_node(root.right, temp.key)
+ 
+    return root
 
 # Driver Code
 if __name__ == '__main__':
@@ -135,8 +193,8 @@ if __name__ == '__main__':
       /	 \ 
     30	 70 
     / \ / \  
-20 40 60 80 
-/
+  20 40 60 80 
+ /
 10   
     """
     root = None
@@ -185,3 +243,16 @@ if __name__ == '__main__':
 
     print("The nodes that are not leaft are the following:")
     print_non_leaf_node(root)
+
+    print("\nDelete 50")
+    root = delete_node(root, 50)
+    print("Inorder Traversal after deletion of 50:", end=" ")
+    inorder(root)
+
+    print("\nDelete 10")
+    root = delete_node(root, 10)
+    print("Inorder Traversal after deletion of 10:", end=" ")
+    inorder(root)
+
+
+
